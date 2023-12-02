ada7e7fca0da6c595fd63921510d5237d6eaa625
sachin vinay <75784102+shinzel1@users.noreply.github.com>
2023-12-02 13:36:34 +0530

    commit

--- src/components/commons/blogPostCards/blogPostCards.js
+++ src/components/commons/blogPostCards/blogPostCards.js
@@ -3,23 +3,8 @@ import React from 'react';
 const BlogPostCards = ({ data }) => {
 	// Sample data for blog listings
 	return (
-		// <article className='blog-article'>
-		// 	<div className="blog-article-wrapper">
-		// 		<figure>
-		// 			<img src={data.imageSrc} alt="" />
-		// 		</figure>
-		// 		<div className="blog-article-body">
-		// 			<h2>{data.title}</h2>
-		// 			<p>
-		// 				{data.description}
-		// 			</p>
-		// 			<a href="#" className="read-more">
-		// 				Read more <span className="sr-only">{data.highlight}</span>
-		// 			</a>
-		// 		</div>
-		// 	</div>
-		// </article>
-		<article className="mb-5">
+	
+		<article className="mb-5" id={data.title}>
               <div className="post-slider slider-sm">
                 <img loading="lazy" src= {data.imageSrc} className="img-fluid" alt="post-thumb" />
                 {/* <img loading="lazy" src={post1} className="img-fluid" alt="post-thumb" />
--- src/components/pages/BlogListings/BlogListings.js
+++ src/components/pages/BlogListings/BlogListings.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect }  from 'react';
 import blogPosts from '../../data/BlogPost.json'
 import BlogPostCards from '../../commons/blogPostCards/blogPostCards';
 import Row from 'react-bootstrap/Row';
@@ -6,19 +6,32 @@ import Col from 'react-bootstrap/Col';
 import '../../commons/blogPostCards/blogPostCards.css'
 import './BlogListings.css'
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom';
 const BlogListings = () => {
   // Sample data for blog listings
+  const search = useLocation().search;
+  const id = new URLSearchParams(search).get("keyId");
+
+  useEffect(() => {
+    // Get the element by ID
+    if (id !== null && id !== "null") {
+      const element = document.getElementById(id);
+      // Trigger a click event on the element
+      if (element) {
+        element.click();
+      }
+    }
+  }, []);
+
+
   return (
     <div>
-      {/* <header className='blogPostHeader'>
-        <h1>Blog and Article Listings</h1>
-      </header> */}
       <div className='BlogListing centered'>
         <Row xs={1} md={4} className="g-4">
           {blogPosts.map((post, index) => (
             <Col key={index}>
               <Link to={'/blogs/' + post.title} state={{ post: post }}>
-                <BlogPostCards key={post.title} data={post} />
+                <BlogPostCards data={post} />
               </Link>
             </Col>
           ))}
