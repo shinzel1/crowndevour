b7c4c2d6a2e1237fa12a88cefc3b76c3fdfe161c
sachin vinay <75784102+shinzel1@users.noreply.github.com>
2024-08-03 17:22:48 +0530

    Update BlogPostDetail.js

--- src/components/pages/BlogPostDetail/BlogPostDetail.js
+++ src/components/pages/BlogPostDetail/BlogPostDetail.js
@@ -72,17 +72,17 @@ function BlogPostDetail() {
 								</div>
 							</div>
 							<div className="col-12 mx-auto">
-								<h1><a className="post-title" href="#">{post?.name}</a></h1>
+								<h1><span className="post-title">{post?.name}</span></h1>
 								<ul className="list-inline post-meta mb-4">
 									<li className="list-inline-item"><i className="ti-user mr-2"></i>
 										<a href="author.html">{post?.author?.name}</a>
 									</li>
 									<li className="list-inline-item">Date : March 15, 2020</li>
-									<li className="list-inline-item">Categories : <a href="#!" className="ml-1">Photography </a>
+									<li className="list-inline-item">Categories : <span className="ml-1">Photography </span>
 									</li>
 									<li className="list-inline-item">Tags :
 										{post?.tags?.slice(0, 3)?.map((item) => (
-											<a href="#!" className="ml-1" onClick={() => fetchTags(item)}>{item} </a>
+											<span className="ml-1" onClick={() => fetchTags(item)}>{item} </span>
 										))}
 									</li>
 								</ul>
@@ -177,7 +177,7 @@ function BlogPostDetail() {
 										</h2>
 										<li className="list-inline-item">Tags :
 											{post?.tags?.slice(0, 3).map((item) => (
-												<a href="#" className="ml-1" onClick={() => fetchTags(item)}>{item} </a>
+												<span className="ml-1" onClick={() => fetchTags(item)}>{item} </span>
 											))}
 										</li>
 									</section>
@@ -204,11 +204,11 @@ function BlogPostDetail() {
 								<Link to={'/location/' + location?.title} state={{ loc: location }}>
 									<ul className="list-unstyled widget-list">
 										<li className="media widget-post align-items-center">
-											<a href="#">
+											<span>
 												<img loading="lazy" className="mr-3" src={location?.imageSrc} alt='image' />
-											</a>
+											</span>
 											<div className="media-body">
-												<h5 className="h6 mb-0"><a href="#">{location?.name}</a></h5>
+												<h5 className="h6 mb-0"><span>{location?.name}</span></h5>
 												<small>{location?.date}</small>
 											</div>
 										</li>
@@ -220,9 +220,9 @@ function BlogPostDetail() {
 							<h5 className="widget-title"><span>Categories</span></h5>
 							<ul className="list-unstyled widget-list">
 								{category.slice(0, 3).map((item, index) => (
-									<li><a href="#!" className="d-flex" onClick={() => fetchCategory(item)}>{item}
+									<li><span className="d-flex" onClick={() => fetchCategory(item)}>{item}
 										<small className="ml-auto">({index})</small>
-									</a>
+									</span>
 									</li>
 								))}
 							</ul>
@@ -231,7 +231,7 @@ function BlogPostDetail() {
 							<h5 className="widget-title"><span>Tags</span></h5>
 							<ul className="list-inline widget-list-inline">
 								{cuisine_tags.map((item, index) => (
-									<li className="list-inline-item" onClick={() => fetchTags(item)}><a href="#!">{item}</a>
+									<li className="list-inline-item" onClick={() => fetchTags(item)}><span>{item}</span>
 									</li>
 								))}
 							</ul>
